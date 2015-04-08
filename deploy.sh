7116c888ba4aa20240da1766af374e2f854d686f
ming.wang <wangmingjob@gmail.com>
2015-04-08 10:49:31 +0800

    update source content

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+simiki generate
+git add .
+git commit -m 'update source content'
+git push origin source:source
+cd output
+git add .
+git commit -m 'update wiki content'
+git push origin master:master
+cd ..
+
